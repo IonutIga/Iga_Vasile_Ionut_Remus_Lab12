3fae4afd86e1ca6526fd96d90acaa0db218c7c4c
VASILE-IONUȚ-REMUS IGA <vasile.iga@stud.ubbcluj.ro>
2021-01-04 19:41:26 +0200

    Add project files.

---
+++ Data/WebAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Iga_Vasile_Ionut_Remus_Lab12.Models;
+
+namespace Iga_Vasile_Ionut_Remus_Lab12.Data
+{
+    public class WebAPIContext : DbContext
+    {
+        public WebAPIContext (DbContextOptions<WebAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Iga_Vasile_Ionut_Remus_Lab12.Models.ShopList> ShopList { get; set; }
+    }
+}
